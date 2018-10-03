87b274a1104f689ba0ed315f40384e6352447771
Konstantinos Bairaktaris <kbairak@transifex.com>
2018-10-03 11:43:56 +0300

    Initial implementation with extra README

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
 # s2arrows
-Interception plugin for mapping s+hjkl keys to arrow keys
+
+Interception plugin for mapping `s`+`hjkl` keys to arrow keys.
+
+<a href="http://www.catonmat.net/blog/why-vim-uses-hjkl-as-arrow-keys/">
+    <img src="http://www.catonmat.net/images/why-vim-uses-hjkl/adm-3a-hjkl-keyboard.jpg" alt="ADM-3A terminal">
+</a>
+
+## What is it?
+
+This is a plugin to migrate the second most useful feature of Karabiner for
+MacOSX to Linux. With this plugin enabled, `s` acts as a modifier key. While
+`s` is pressed, the `h` `j` `k` `l` keys act as arrow keys, following the VI
+key bindings.
+
+## Why?
+
+It's so nice in VIM that you don't have to lift your right hand to move to the
+arrow keys. It would be great if you could do the same in all applications. In
+fact, once you get used to this, it's very hard to go back.
+
+## Dependencies
+
+- [Interception Tools][interception-tools]
+
+## Building
+
+```sh
+git clone git@github.com:kbairak/s2arrows
+cd s2arrows
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+```
+
+## Enabling
+
+### udevmon
+
+`s2arrows` is an [_Interception Tools_][interception-tools] plugin. A suggested
+`udevmon` job configuration (`/etc/udevmon.yaml`) is:
+
+```yaml
+- JOB: "intercept -g $DEVNODE | s2arrows | uinput -d $DEVNODE"
+  DEVICE:
+    EVENTS:
+      EV_KEY: [KEY_S, KEY_H, KEY_J, KEY_K, KEY_K, KEY_LEFT, KEY_DOWN,
+               KEY_UP, KEY_RIGHT]
+```
+
+If you already have an [_Interception Tools_][interception-tools] plugin
+running, like, I don't know, [caps2esc][caps2esc], you should pipe the two
+commands to each other and append the keys to the list instead:
+
+```yaml
+- JOB: "intercept -g $DEVNODE | caps2esc | s2arrows | uinput -d $DEVNODE"
+  DEVICE:
+    EVENTS:
+      EV_KEY: [KEY_CAPSLOCK, KEY_ESC, KEY_S, KEY_H, KEY_J, KEY_K, KEY_K,
+               KEY_LEFT, KEY_DOWN, KEY_UP, KEY_RIGHT]
+```
+
+### systemd
+
+A suggested systemd service to run udevmon can be setup by creating the
+following file (`/etc/systemd/udevmon.service`):
+
+```ini
+[Unit]
+Description=udevmon
+After=systemd-user-sessions.service
+
+[Service]
+ExecStart=/usr/bin/nice -n -20 usr/local/bin/udevmon -c /etc/udevmon.yaml
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Then, you can enable and start the service like any other:
+
+```sh
+sudo systemctl enable udevmon.service
+sudo systemctl start udevmon.service
+```
+
+## Caveats
+
+The main problem with using this plugin is that the `s` key might be
+interpreted as a modifier, when in fact you actually want to press `s`. This
+can happen when you type something like `ssh some_host` very fast. The `sh`
+part from the sentence might be interpreted as a press of the left arrow key
+and the end result may appear as ` some_hosts`. The same problem used to happen
+with Karabiner too. Over time, you will get used to letting an extra
+millisecond pass after every `s` key press.
+
+Another caveat is that with this plugin enabled, you lose the ability to hold
+the `s` key down and have the character repeat itself. From my experience, this
+has never been a problem for me.
+
+## TODOs
+
+- Map `s`+`f`+`h` `j` `k` `l` to `home`, `page down`, `page up` and `end`
+
+## License
+
+<a href="https://gitlab.com/interception/linux/plugins/caps2esc/blob/master/LICENSE.md">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/License_icon-mit-2.svg/120px-License_icon-mit-2.svg.png" alt="MIT">
+</a>
+
+[caps2esc]: https://gitlab.com/interception/linux/plugins/caps2esc
+[interception-tools]: https://gitlab.com/interception/linux/tools
---
+++ s2arrows.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <linux/input.h>
+
+// clang-format off
+const struct input_event
+    s_up     = {.type = EV_KEY, .code = KEY_S, .value = 0},
+    s_down   = {.type = EV_KEY, .code = KEY_S, .value = 1},
+    s_repeat = {.type = EV_KEY, .code = KEY_S, .value = 2},
+
+    h_up     = {.type = EV_KEY, .code = KEY_H, .value = 0},
+    h_down   = {.type = EV_KEY, .code = KEY_H, .value = 1},
+    h_repeat = {.type = EV_KEY, .code = KEY_H, .value = 2},
+    j_up     = {.type = EV_KEY, .code = KEY_J, .value = 0},
+    j_down   = {.type = EV_KEY, .code = KEY_J, .value = 1},
+    j_repeat = {.type = EV_KEY, .code = KEY_J, .value = 2},
+    k_up     = {.type = EV_KEY, .code = KEY_K, .value = 0},
+    k_down   = {.type = EV_KEY, .code = KEY_K, .value = 1},
+    k_repeat = {.type = EV_KEY, .code = KEY_K, .value = 2},
+    l_up     = {.type = EV_KEY, .code = KEY_L, .value = 0},
+    l_down   = {.type = EV_KEY, .code = KEY_L, .value = 1},
+    l_repeat = {.type = EV_KEY, .code = KEY_L, .value = 2},
+
+    left_up      = {.type = EV_KEY, .code = KEY_LEFT,  .value = 0},
+    left_down    = {.type = EV_KEY, .code = KEY_LEFT,  .value = 1},
+    left_repeat  = {.type = EV_KEY, .code = KEY_LEFT,  .value = 2},
+    down_up      = {.type = EV_KEY, .code = KEY_DOWN,  .value = 0},
+    down_down    = {.type = EV_KEY, .code = KEY_DOWN,  .value = 1},
+    down_repeat  = {.type = EV_KEY, .code = KEY_DOWN,  .value = 2},
+    up_up        = {.type = EV_KEY, .code = KEY_UP,    .value = 0},
+    up_down      = {.type = EV_KEY, .code = KEY_UP,    .value = 1},
+    up_repeat    = {.type = EV_KEY, .code = KEY_UP,    .value = 2},
+    right_up     = {.type = EV_KEY, .code = KEY_RIGHT, .value = 0},
+    right_down   = {.type = EV_KEY, .code = KEY_RIGHT, .value = 1},
+    right_repeat = {.type = EV_KEY, .code = KEY_RIGHT, .value = 2},
+
+    syn = {.type = EV_SYN, .code = SYN_REPORT, .value = 0};
+// clang-format on
+
+int equal(const struct input_event *first, const struct input_event *second) {
+    return first->type == second->type && first->code == second->code &&
+           first->value == second->value;
+}
+
+int read_event(struct input_event *event) {
+    return fread(event, sizeof(struct input_event), 1, stdin) == 1;
+}
+
+void write_event(const struct input_event *event) {
+    if (fwrite(event, sizeof(struct input_event), 1, stdout) != 1)
+        exit(EXIT_FAILURE);
+}
+
+int main(void) {
+    int s_is_down = 0, s_give_up = 0;
+    struct input_event input;
+    setbuf(stdin, NULL), setbuf(stdout, NULL);
+
+    while (read_event(&input)) {
+        // Fluff
+        if (input.type == EV_MSC && input.code == MSC_SCAN) {
+            continue;
+        }
+        if (input.type != EV_KEY) {
+            write_event(&input);
+            continue;
+        }
+
+        if (equal(&input, &s_down)) {
+            // 's' was pressed, mark and supress
+            s_is_down = 1;
+            continue;
+        }
+
+        if (s_is_down) {
+            // Something else happened while 's' was down
+            if (equal(&input, &s_down) || equal(&input, &s_repeat)) {
+                // More 's', suppress
+                continue;
+            }
+
+            if (equal(&input, &s_up)) {
+                // 's' was released
+                if (! s_give_up) {
+                    // Nothing else happened in the meantime, process 's'
+                    // normally
+                    write_event(&s_down);
+                    write_event(&syn);
+                    usleep(20000);
+                    write_event(&s_up);
+                }
+                s_is_down = 0;
+                s_give_up = 0;
+                continue;
+            }
+
+            // Something other than 's' happened
+            s_give_up = 1;
+
+            const struct input_event *mappings[12][2] = {
+                {&h_up,     &left_up},
+                {&h_down,   &left_down},
+                {&h_repeat, &left_repeat},
+                {&j_up,     &down_up},
+                {&j_down,   &down_down},
+                {&j_repeat, &down_repeat},
+                {&k_up,     &up_up},
+                {&k_down,   &up_down},
+                {&k_repeat, &up_repeat},
+                {&l_up,     &right_up},
+                {&l_down,   &right_down},
+                {&l_repeat, &right_repeat},
+            };
+
+            int broke = 0;
+            for (int i = 0; i < 12; i++) {
+                const struct input_event *from = mappings[i][0],
+                                         *to   = mappings[i][1];
+                if (equal(&input, from)) {
+                    write_event(to);
+                    write_event(&syn);
+                    usleep(20000);
+                    broke = 1;
+                    break;
+                }
+            }
+            if (broke) {
+                continue;
+            }
+        }
+
+        write_event(&input);
+    }
+}
